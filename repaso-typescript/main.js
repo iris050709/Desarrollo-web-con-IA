dfc0c68545ef089e9376e3d2db108b3ecb977387
baruwrx <al222310918@gmail.com>
2025-04-29 13:32:07 -0600

    REPASO TYPESCRIPT PARTE 1

---
+++ repaso-typescript/main.js
@@ -0,0 +1,8 @@
+var texto = "HOLA";
+var numero = 12;
+var verdadero = true;
+var cualquier = "puedo meter lo que sea";
+verdadero = false;
+//CONSTANTES
+var nombre = "IRIS";
+console.log(texto, numero, verdadero);
---
+++ repaso-typescript/main.ts
@@ -0,0 +1,15 @@
+let texto:string = "HOLA";
+
+let numero:number = 12;
+
+let verdadero:boolean = true;
+
+let cualquier:any = "puedo meter lo que sea";
+
+verdadero = false;
+
+//CONSTANTES
+
+const nombre:string ="IRIS";
+
+console.log(texto, numero, verdadero);
